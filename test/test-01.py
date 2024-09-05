4ffe7c81049d0396d989b2529f4fe246dced5145
Henrique Matos <henriquematosteixeira@outlook.com>
2024-09-05 00:08:40 -0300

    Fix: Ajustes finos

--- test/test-01.py
+++ test/test-01.py
@@ -1,5 +1,8 @@
 import pygame
 import sys
+import yt_dlp
+import os
+import stat
 
 pygame.init()
 
@@ -20,21 +23,57 @@ musica = ''
 music_file = ''
 volume = 1.0
 
-# Gêneros e Músicas organizados em dicionários
-generos = {
-    '1': 'Drill',
-    '2': 'Corrido Tumbado',
-    '3': 'RnB'
+# URLs de vídeos do YouTube
+musicas_por_genero = {
+    '1': [('Doja - Central Cee', 'https://www.youtube.com/watch?v=_VuJA-VQRcY&pp=ygUQY2VudHJhbCBjZWUgZG9qYQ%3D%3D'),
+          ('Irish Drill - AC-130', 'https://www.youtube.com/watch?v=uyHUPhWYRsE&pp=ygUUSXJpc2ggRHJpbGwgLSBBQy0xMzA%3D')],
+    '2': [('Bizarrap Session - Peso Pluma', 'https://www.youtube.com/watch?v=v5_SYkFpFiY&pp=ygUdQml6YXJyYXAgU2Vzc2lvbiAtIFBlc28gUGx1bWE%3D'),
+          ('TQM - Fuerza Regida', 'https://www.youtube.com/watch?v=DzTNN5Zzpok&pp=ygUTVFFNIC0gRnVlcnphIFJlZ2lkYQ%3D%3D')],
+    '3': [('Ivy - Frank Ocean', 'https://www.youtube.com/watch?v=AE005nZeF-A&pp=ygURSXZ5IC0gRnJhbmsgT2NlYW4%3D'),
+          ('After Hours - The Weeknd', 'https://www.youtube.com/watch?v=ygTZZpVkmKg&pp=ygUYQWZ0ZXIgSG91cnMgLSBUaGUgV2Vla25k')]
 }
 
-musicas_por_genero = {
-    '1': [('Doja - Central Cee', './ytmp3-converter.com_320kbps-central-cee-doja-official-music-video.mp3'),
-          ('Irish Drill - AC-130', './ytmp3-converter.com_320kbps-irish-drill.mp3')],
-    '2': [('Bizarrap Session - Peso Pluma', './ytmp3-converter.com_320kbps-peso-pluma-bzrp-music-sessions-55.mp3'),
-          ('TQM - Fuerza Regida', './ytmp3-converter.com_320kbps-fuerza-regida-tqm.mp3')],
-    '3': [('Ivy - Frank Ocean', './ytmp3-converter.com_320kbps-frank-ocean-ivy.mp3'),
-          ('After Hours - The Weeknd', './ytmp3-converter.com_320kbps-the-weeknd-after-hours-audio.mp3')]
-}
+# Baixar áudio usando yt-dlp
+def download_youtube_audio(url):
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'outtmpl': 'temp_audio',
+        'ffmpeg_location': r'C:\ffmpeg\bin',  # Caminho para o FFmpeg, se necessário
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+
+    return 'temp_audio.mp3'
+
+def play_music_from_youtube(url):
+    global music_file
+    # Baixa o áudio do YouTube
+    music_file = download_youtube_audio(url)
+    # Carrega e toca o arquivo baixado
+    pygame.mixer.music.load(music_file)
+    pygame.mixer.music.set_volume(volume)
+    pygame.mixer.music.play()
+
+def delete_temp_audio():
+    # Certifique-se de que a música foi interrompida antes de tentar deletar
+    if pygame.mixer.music.get_busy():
+        pygame.mixer.music.stop()  # Para a música
+    
+    # Espera um pouco para garantir que o arquivo foi liberado
+    pygame.mixer.music.unload()  # Garante que o arquivo foi completamente descarregado do mixer
+    
+    # Tenta remover o arquivo após garantir que ele está liberado
+    temp_audio_path = 'temp_audio.mp3'
+    
+    if os.path.exists(temp_audio_path):
+        os.chmod(temp_audio_path, stat.S_IWRITE)
+        os.remove(temp_audio_path)
 
 # Criando uma classe de botão
 class Button:
@@ -92,6 +131,45 @@ buttons_menu = [
     Button("Sair", 50, 50, 100, 50, GRAY, DARK_GRAY, quit_program)
 ]
 
+def select_music_1():
+    delete_temp_audio()
+    global musica, music_menu
+    musica = '1'
+    music_menu = True
+    musica, music_url = musicas_por_genero[genero][0]
+    print(musica, music_url)
+    play_music_from_youtube(music_url)
+
+def select_music_2():
+    delete_temp_audio()
+    global musica, music_menu
+    musica = '2'
+    music_menu = True
+    musica, music_url = musicas_por_genero[genero][1]
+    print(musica, music_url)
+    play_music_from_youtube(music_url)
+
+def voltar():
+    global menu, music_menu
+    menu = True
+    music_menu = False
+
+buttons_music_menu = [[
+    Button("1 - Doja - Central Cee", 100, 200, 200, 50, GRAY, DARK_GRAY, select_music_1),
+    Button("2 - Irish Drill - AC-130", 100, 300, 200, 50, GRAY, DARK_GRAY, select_music_2),
+    Button("Q - Sair", 50, 50, 100, 50, GRAY, DARK_GRAY, voltar)
+],
+[
+    Button("3 - Bizarrap Session - Peso Pluma", 100, 200, 200, 50, GRAY, DARK_GRAY, select_music_1),
+    Button("4 - TQM - Fuerza Regida", 100, 300, 200, 50, GRAY, DARK_GRAY, select_music_2),
+    Button("Q - Sair", 50, 50, 100, 50, GRAY, DARK_GRAY, voltar)
+],
+[
+    Button("5 - Ivy - Frank Ocean", 100, 200, 200, 50, GRAY, DARK_GRAY, select_music_1),
+    Button("6 - After Hours - The Weeknd", 100, 300, 200, 50, GRAY, DARK_GRAY, select_music_2),
+    Button("Q - Sair", 50, 50, 100, 50, GRAY, DARK_GRAY, voltar)
+]]
+
 def draw_menu():
     screen.fill(WHITE)
     text = font.render('Selecione o genero musical:', True, BLACK)
@@ -105,14 +183,11 @@ def draw_music_menu():
     screen.fill(WHITE)
     text = font.render('Selecione uma musica:', True, BLACK)
     screen.blit(text, (100, 100))
-    if genero in musicas_por_genero:
-        for idx, (nome_musica, _) in enumerate(musicas_por_genero[genero]):
-            text = font.render(f'{idx + 1} - {nome_musica}', True, BLACK)
-            screen.blit(text, (100, 200 + idx * 50))
-        text = font.render('P - Play/Pause / Setas - Volume', True, BLACK)
-        screen.blit(text, (100, 300 + len(musicas_por_genero[genero]) * 50))
-        text = font.render('Q - Voltar', True, BLACK)
-        screen.blit(text, (50, 50))
+    for button in buttons_music_menu[int(genero) - 1]:
+        button.draw(screen)
+        
+    text = font.render('SPACE - Play/Pause / Setas - Volume', True, BLACK)
+    screen.blit(text, (100, 300 + len(musicas_por_genero[genero]) * 50))
     pygame.display.update()
 
 def play_pause_music():
@@ -130,6 +205,7 @@ def play_music(music_file):
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+            delete_temp_audio()
             quit_program()
 
         if menu:
@@ -140,18 +216,11 @@ while True:
 
         elif music_menu:
             if event.type == pygame.KEYDOWN:
-                if event.key in [pygame.K_1, pygame.K_2]:
-                    musica_idx = event.key - pygame.K_1
-                    if musica_idx < len(musicas_por_genero[genero]):
-                        musica, music_file = musicas_por_genero[genero][musica_idx]
-                elif event.key == pygame.K_p and music_file:
-                    if pygame.mixer.music.get_busy():
-                        play_pause_music()
-                    else:
-                        play_music(music_file)
-                elif event.key == pygame.K_q:
-                    music_menu = False
-                    menu = True
+                if event.key == pygame.K_SPACE and music_file:
+                    play_pause_music()
+            for button in buttons_music_menu[int(genero) - 1]:
+                if button.is_clicked(event):
+                    break
 
     # Desenha o menu ou o menu de música
     if menu:
